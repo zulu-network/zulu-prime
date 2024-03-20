f96564836ac4cb6abb794809bafaeeb272da469e
Paul Cheng <chengyuejia@foxmail.com>
2024-03-20 10:28:28 +0800

    bugfix

--- core/lib/dal/src/blocks_web3_dal.rs
+++ core/lib/dal/src/blocks_web3_dal.rs
@@ -685,7 +685,7 @@ impl BlocksWeb3Dal<'_, '_> {
                 l1_batches.default_aa_code_hash,
                 offchain_verfication.status AS "offchain_verfication_status",
                 offchain_verfication.verifier_picked_at AS "offchain_verifier_picked_at?",
-                offchain_verfication.verifier_submit_at AS "offchain_verifier_submit_at?",
+                offchain_verfication.verifier_submit_at AS "offchain_verifier_submit_at?"
             FROM
                 l1_batches
                 INNER JOIN mb ON TRUE
